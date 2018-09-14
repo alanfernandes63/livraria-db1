1738fe7295627433e0c8c90fe05dbd71e912f5da
Aluno <alan.fernandes63@gmail.com>
2018-09-14 16:11:36 -0300

    inserção de dados

--- app/src/main/java/br/eaj/tads/livraria_db1/util/BancoHelper.java
+++ app/src/main/java/br/eaj/tads/livraria_db1/util/BancoHelper.java
@@ -1,19 +1,24 @@
 package br.eaj.tads.livraria_db1.util;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import br.eaj.tads.livraria_db1.model.Livro;
 
 public class BancoHelper extends SQLiteOpenHelper {
 
     //Strings auxiliares
     private static String TAG = "sql";
-    private static String TEXT = "TEXT";
-    private static String INTEGER = "INTEGER";
+    private static String TEXT = " TEXT";
+    private static String INTEGER = " INTEGER";
     private static String VIRGULA = ",";
+    private static String REAL = " REAL";
 
     //Nome do banco
     private static String NOME_BANCO = "livraria_db";
@@ -25,7 +30,7 @@ public class BancoHelper extends SQLiteOpenHelper {
     private static String CREATE_TABLE = ("CREATE TABLE " + LivroContrato.LivroEntry.TABLE_NAME +
     "(" + LivroContrato.LivroEntry._ID  + INTEGER + VIRGULA + LivroContrato.LivroEntry.TITULO + TEXT +
     VIRGULA + LivroContrato.LivroEntry.AUTOR + TEXT + VIRGULA + LivroContrato.LivroEntry.ANO + INTEGER +
-    VIRGULA + LivroContrato.LivroEntry.NOTA + INTEGER + ");");
+    VIRGULA + LivroContrato.LivroEntry.NOTA + REAL + ");");
 
     private static String DROP_TABLE = ("DROP TABLE " + LivroContrato.LivroEntry.TABLE_NAME);
 
@@ -50,4 +55,40 @@ public class BancoHelper extends SQLiteOpenHelper {
             this.onCreate(banco);
         }
     }
+    // metoto para salvar um registro no bamco
+    public long insert(Livro livro) {
+
+        long id = livro.getId();
+        SQLiteDatabase banco = getWritableDatabase();
+
+
+        try {
+
+            ContentValues valores = new ContentValues();
+            valores.put(LivroContrato.LivroEntry.TITULO, livro.getTitulo());
+            valores.put(LivroContrato.LivroEntry.AUTOR, livro.getAutor());
+            valores.put(LivroContrato.LivroEntry.ANO, livro.getAno());
+            valores.put(LivroContrato.LivroEntry.NOTA, livro.getNota());
+
+
+
+            if (id != 0) {
+                String selecao = LivroContrato.LivroEntry._ID + "= ?";
+                String[] stringId = new String[]{String.valueOf(LivroContrato.LivroEntry._ID)};
+
+                //atualizar carro cujo id seja = _ID = ?
+                int count = banco.update(LivroContrato.LivroEntry.TABLE_NAME, valores, selecao, stringId);
+                Log.i(TAG, "Atualizou! id = " + id + " Tabela " + LivroContrato.LivroEntry.TABLE_NAME + "Banco: " + NOME_BANCO);
+                return count;
+            } else {
+                //insert
+                id = banco.insert(LivroContrato.LivroEntry.TABLE_NAME, "", valores);
+                Log.i(TAG, "Inseriu id = " + id + "Tabela: " + LivroContrato.LivroEntry.TABLE_NAME + "Banco: " + NOME_BANCO);
+                return id;
+            }
+        }finally{
+           banco.close();
+        }
+    }
+
 }
--- app/src/main/java/br/eaj/tads/livraria_db1/view/CadastroLivro.java
+++ app/src/main/java/br/eaj/tads/livraria_db1/view/CadastroLivro.java
@@ -1,15 +1,48 @@
 package br.eaj.tads.livraria_db1.view;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import br.eaj.tads.livraria_db1.R;
+import br.eaj.tads.livraria_db1.model.Livro;
+import br.eaj.tads.livraria_db1.util.BancoHelper;
 
 public class CadastroLivro extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_livro);
+
+    }
+
+
+    public void cadastrarLivro(View v){
+
+        EditText tituloEdit = findViewById(R.id.titulo);
+        EditText autorEdit = findViewById(R.id.autor);
+        EditText anoEdit = findViewById(R.id.ano);
+        RatingBar notaRanting = findViewById(R.id.nota);
+
+        BancoHelper db = new BancoHelper(this);
+
+        String titulo = tituloEdit.getText().toString();
+        String autor = autorEdit.getText().toString();
+        int ano = Integer.parseInt(anoEdit.getText().toString());
+        float nota = notaRanting.getRating();
+        int id = 0;
+
+        Livro livro = new Livro(
+                titulo
+                ,autor
+                ,ano,nota,id);
+
+        Toast.makeText(this, "" + id, Toast.LENGTH_SHORT).show();
+        id++;
+        db.insert(livro);
     }
 }
